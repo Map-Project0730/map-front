7bd21b7f44625ff9cc5585f48e943668201f61e8
rlaalstjd0310 <minsung74310@gmail.com>
2025-08-15 21:50:40 +0900

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,18 @@
+import { useNavigate } from 'react-router-dom';
+
+function Header() {
+  const navigate = useNavigate();
+
+  return (
+    <header className="flex justify-between px-4 py-5 font-sans text-3xl text-white bg-black">
+      <h1 className="font-semibold">MAP</h1>
+      <div className="flex gap-6">
+        <p className="cursor-pointer hover:underline" onClick={() => navigate('/home')}>HOME</p>
+        <p className="cursor-pointer hover:underline" onClick={() => navigate('/')}>NOTICE</p>
+        <p className="cursor-pointer hover:underline" onClick={() => navigate('/mypage')}>MYPAGE</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ WritePage.jsx
@@ -0,0 +1,63 @@
+import Header from '../components/Header';
+import { useState } from 'react';
+
+function WritePage() {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleFileUpload = () => {
+    alert(' ');
+  };
+
+  const handleSubmit = () => {
+    alert(`제목: ${title}\n내용: ${content}`);
+  };
+
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+
+      <div>
+        <h1 className="px-5 py-5 font-bold text-7xl">NOTICE</h1>
+      </div>
+
+      <div className="flex items-center py-4 px-10 gap-6">
+        <h1 className="text-5xl font-bold w-40">TITLE</h1>
+        <input
+          type="text"
+          placeholder="제목을 입력하세요"
+          className="py-3 px-3 text-3xl text-white bg-black rounded-full w-[1200px] ml-auto"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+
+      <div className="flex items-start py-4 px-10 gap-6">
+        <h1 className="text-5xl font-bold w-40">CONTENT</h1>
+        <textarea
+          placeholder="내용을 입력하세요"
+          className="w-[1200px] h-[270px] text-2xl text-white bg-black rounded px-3 py-2 ml-auto"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+      </div>
+
+      <div className="flex justify-between px-10 py-4 gap-4">
+        <button
+          className="px-6 py-2 text-3xl text-white bg-black rounded cursor-pointer"
+          onClick={handleFileUpload}
+        >
+          FILE
+        </button>
+        <button
+          className="px-6 py-2 text-3xl text-white bg-black rounded cursor-pointer"
+          onClick={handleSubmit}
+        >
+          UPLOAD
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default WritePage;
